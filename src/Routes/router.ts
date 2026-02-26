89e254d109ac17215c633dc32a5812d58bb0c877
DANILO ALMEIDA CORREA <danilo.acorrea@senacsp.edu.br>
2026-02-26 14:03:22 -0300

    correçoes

--- src/Routes/router.ts
+++ src/Routes/router.ts
@@ -1,34 +1,19 @@
 import { Router } from "express";
 
-import rotaLogin from "./loginRouter";
-import { createJWT } from "../utils/jwt";
 import { middleware } from "./jwtMiddleware";
 
+import rotaLogin from "./loginRouter";
 import rotaQuartos from "./quartosRouter";
 import rotaReservas from "./reservaRouter";
 
 const handlerRouter = Router();
 
 // rotas publicas
-
 handlerRouter.use("/api/login", rotaLogin);
 handlerRouter.use("/api/quartosDisponiveis", rotaQuartos);
 
 // rotas privadas
 handlerRouter.use("/api/reserva", middleware, rotaReservas);
-
-
-handlerRouter.use("/jwt", (req, res)=>{
-    const payload = {
-        id: 123,
-        nome: "fulano",
-        cargo: "cliente"
-    }
-    res.json(createJWT(payload))
-})
-handlerRouter.get("/testeJWT", middleware, (req, res)=>{
-    res.json("passou pelo JWT middleware")
-})
 
 
 
