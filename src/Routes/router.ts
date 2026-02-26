c620bc7e9b45da3c4899f22e2b680ee521022692
DANILO ALMEIDA CORREA <danilo.acorrea@senacsp.edu.br>
2026-02-26 14:48:55 -0300

    agora vai 1.2

--- src/Routes/router.ts
+++ src/Routes/router.ts
@@ -3,12 +3,14 @@ import { Router } from "express";
 import { middleware } from "./jwtMiddleware";
 
 import rotaLogin from "./loginRouter";
+
 import rotaQuartos from "./quartosRouter";
 import rotaReservas from "./reservaRouter";
 
 const handlerRouter = Router();
 
 // rotas publicas
+
 handlerRouter.use("/api/login", rotaLogin);
 handlerRouter.use("/api/quartosDisponiveis", rotaQuartos);
 
